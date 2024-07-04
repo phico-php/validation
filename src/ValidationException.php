84190da69bbc71d3d1221db7d923c10f6d153795
indgy <65541311+indgy@users.noreply.github.com>
2024-07-04 18:07:44 +0100

    Added ValidationException

---
+++ src/ValidationException.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phico\View;
+
+use Phico\PhicoException;
+use Somnambulist\Components\Validation\Validation;
+
+
+class ValidationException extends PhicoException
+{
+    protected Validation $validator;
+
+    public function __construct(string $message, Validation $validator)
+    {
+        parent::__construct($message, 422);
+        $this->validator = $validator;
+    }
+    public function getInput()
+    {
+        return $this->validator->input();
+    }
+    public function getErrors()
+    {
+        return $this->validator->errors();
+    }
+    public function getFailed()
+    {
+        return $this->validator->getInvalidData();
+    }
+    public function getPassed()
+    {
+        return $this->validator->getValidData();
+    }
+    public function toArray(): array
+    {
+        return [
+            'code' => $this->getCode(),
+            'message' => $this->getMessage(),
+            'errors' => $this->getErrors(),
+            'input' => $this->getInput(),
+            'passed' => $this->getPassed(),
+            'failed' => $this->getFailed(),
+        ];
+    }
+}
